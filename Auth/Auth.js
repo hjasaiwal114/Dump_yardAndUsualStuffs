d9a55e3fd03fb22f26128ef11cf1b2e44432661c
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-09-06 18:21:18 +0530

    installing json web token

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,32 +1,31 @@
 const User = require("../model/User")
+const bcrypt = require("bcryptjs")
 
 exports.register = async (req, res, next) => {
-    const { username , password } = req.body;
-    //  Check if the password length is less than 6 characters
-
+    const { username, password } = req.body;
     if (password.length < 6) {
-        return res.status(400).json({message: "Password less than 6 characters" });
+        return res.status(400).json({ message: "password less than 6 characters" })
     }
-
-    try {
-        // Attempt to create a new user using the privided username and password
-        await User.create({
-            username,
-            password,
-        }).then((user) => 
-           res.status(200).json({
-            messgae: "user successfully created",
-            user,
-           })
-        );
-    } catch (err) {
-        // Handle any errors that occu during user creation
-        res.status(401).json({
+  
+    bcrypt.hash(password, 10).then(async (hash) => {
+      await User.create({
+        username,
+        password: hash,
+      })
+        .then((user) =>
+          res.status(200).json({
+            message: "User successfully created",
+             user,
+          })
+        )
+        .catch((error) =>
+          res.status(400).json({
             message: "User not successful created",
-            error: err.message,
-        });
-    }
-};
+            error: error.message,
+          })
+        );
+    });
+  };
 
 exports.login = async (req, res, next) => {
     const { username, password} = req.body
