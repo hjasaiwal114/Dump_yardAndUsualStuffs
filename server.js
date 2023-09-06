daed5452bfe7d6ffad48bf6b948130f8c60aad7c
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-09-06 12:59:14 +0530

    creating schema

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express()
+const PORT = 5000
+const connectDB = require("./db");
+
+const server = app.listen(PORT, () => {
+    console.log(`Server COnnected to port ${PORT}`)
+})
+// Handling error
+process.on("unhandeledRejection", err => {
+    console.log(`An error occured: ${err.message}`)
+    server.close(() => process.exist(1))
+})
+
+app.listen(PORT, () => console.log(`Server connected to prt ${PORT}`))
+connectDB();
