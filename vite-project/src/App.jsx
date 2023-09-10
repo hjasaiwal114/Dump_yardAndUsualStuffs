9a18627a2123fe06f7eb1782c043f4d9b461a1b7
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-09-10 11:57:50 +0530

    breaking down into component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,39 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./styles.css"
 
-// var todos =[ {
-// titlle: "go to gym",
-// description: "go to gym at 11",
-// id: 1
-// }, {
-// title: "go eat food",
-// description: "go eat food",
-// id: 1
-// },]
 
-function App() {
+export default function App() {
+  const [newItem, setNewItem] = useState("") 
+  const [todos, setTodos ]  = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID, title: newItem, completed: false },
+      ]
+    })
+  }
+  console.log(todos)
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return ( <div styleName={styles.todos}></div>, completed )
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id, e.target.checked)
+    })
+  }
 
   return (
-    <div>
-      <HelloWorldComponent></HelloWorldComponent>
-      <HelloWorldComponent2></HelloWorldComponent2>
+    <>
+     <form onSubmit={handleSubmit} className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input 
+        value={newItem}
+        onChange={e => setNewItem(e.target.value)}
+        type="text"
+        id="item"
+      />
     </div>
+    < button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      {todos.length === 0 && "No todos"}
+      {todos.map(todo => {
+        return (
+          <li key={todo.id}>
+            <label>
+            <input type="checkbox" checked={todo.completed}
+            onChange={e => toggleTodo(todo.id, e.target.checked)}
+               />
+              {todo.title}
+            </label>
+            <button 
+              onClick={() => deleteTodo(todo.id)}
+              className="btn btn-danger">Delete</button>
+          </li>
+        )
+      })}
+        
+    </ul>
+    </>
   )
-
 }
-
-function HelloWorldComponent() {
-  return <div>
-    hello world
-  </div>
-}
-
-function HelloWorldComponent() {
-  return <div>
-    hello world 2
-  </div>
-}
-
-export default App
